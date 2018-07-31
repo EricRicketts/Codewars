aa6866230507fb2166d62d086ba81f47f60a2cd1
EricRicketts <eric_ricketts@me.com>
2018-07-31 06:05:21 -0400

    two more level 7 katas from Codewars

---
+++ ruby/7kyu/inverting_a_hash.rb
@@ -0,0 +1,130 @@
+=begin
+Inverting A Hash
+
+Summary
+Given a Hash made up of keys and values, invert the hash by swapping them.
+
+Examples
+Given:
+
+  { 'a' => 1,
+    'b' => 2,
+    'c' => 3 }
+
+Return:
+
+  { 1 => 'a',
+    2 => 'b',
+    3 => 'c' }
+
+
+
+Given:
+
+  { 'foo'   => 'bar',
+    'hello' => 'world' }
+
+Return:
+
+  { 'bar'   => 'foo',
+    'world' => 'hello' }
+Notes
+Keys and values may be of any type appropriate for use as a key.
+All hashes provided as input will have unique values, so the inversion is involutive.
+In other words, do not worry about identical values stored under different keys.
+Ruby has a built-in Hash#invert. This is awesome, but is disabled for this kata
+so you can solve it yourself.
+=end
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry-byebug'
+
+class InvertingAHashManual < Minitest::Test
+
+=begin
+I: hash
+O: hash
+AL:
+  - create an empty hash
+  - cycle through each element of the current hash
+  - populate the new hash with hash[value] = key
+  - return the new hash
+=end
+
+  def invert_hash(hash)
+    h = {}
+    hash.each do |k, v|
+      h[v] = k
+    end
+    h
+  end
+
+  def test_1
+    # skip
+    input = { 'hello' => 'world' }
+    expected = { 'world' => 'hello' }
+    assert_equal(expected, invert_hash(input))
+  end
+
+  def test_2
+    # skip
+    input = { 'a' =>  1, 'b' => 2, 'c' => 3  }
+    expected = {  1  => 'a', 2 => 'b', 3 => 'c' }
+    assert_equal(expected, invert_hash(input))
+  end
+
+end
+
+class InvertingAHashSecondSolution < Minitest::Test
+
+  def invert_hash(hash)
+    hash.to_a.map { |arr| arr.reverse }.to_h
+  end
+
+  def test_1
+    # skip
+    input = { 'hello' => 'world' }
+    expected = { 'world' => 'hello' }
+    assert_equal(expected, invert_hash(input))
+  end
+
+  def test_2
+    # skip
+    input = { 'a' =>  1, 'b' => 2, 'c' => 3  }
+    expected = {  1  => 'a', 2 => 'b', 3 => 'c' }
+    assert_equal(expected, invert_hash(input))
+  end
+
+end
+
+class InvertingAHashCodewarsGood < Minitest::Test
+
+=begin
+I had the same idea but this solution showed a greater
+knowledge of how #map works on hashes.  When #map is called
+on a hash the block variable is an array [key, value], further
+calling #map on a hash returns an array of arrays =>
+h = { 'a' =>  1, 'b' => 2, 'c' => 3  }
+h.map(&:reverse) = [[1, "a"], [2, "b"], [3, "c"]]
+=end
+
+  def invert_hash(hash)
+    hash.map(&:reverse).to_h
+  end
+
+  def test_1
+    # skip
+    input = { 'hello' => 'world' }
+    expected = { 'world' => 'hello' }
+    assert_equal(expected, invert_hash(input))
+  end
+
+  def test_2
+    # skip
+    input = { 'a' =>  1, 'b' => 2, 'c' => 3  }
+    expected = {  1  => 'a', 2 => 'b', 3 => 'c' }
+    assert_equal(expected, invert_hash(input))
+  end
+
+end
---
+++ ruby/7kyu/sub_array_elements_sum.rb
@@ -0,0 +1,71 @@
+=begin
+Sub-array elements sum
+
+Given an array (or list or vector) of arrays (or, guess what, lists or vectors) of integers, your goal is to return the sum of a specific set of numbers,
+starting with elements whose position is equal to the main array length and going down by one at each step.
+
+Say for example the parent array (etc, etc) has 3 sub-arrays inside: you should consider the third element of the first sub-array,
+the second of the second array and the first element in the third one: [[3, 2, 1, 0], [4, 6, 5, 3, 2], [9, 8, 7, 4]] would have you
+considering 1 for [3, 2, 1, 0], 6 for [4, 6, 5, 3, 2] and 9 for [9, 8, 7, 4], which sums up to 16.
+
+One small note if that not always each sub-array will have enough elements, in which case you should then use a default value
+if provided or 0 if not provided, as shown in the test cases.
+
+describe "Basic tests" do
+  Test.assert_equals(elements_sum([[3, 2, 1, 0], [4, 6, 5, 3, 2], [9, 8, 7, 4]]), 16)
+  Test.assert_equals(elements_sum([[3], [4, 6, 5, 3, 2], [9, 8, 7, 4]]), 15)
+  Test.assert_equals(elements_sum([[3, 2, 1, 0], [4, 6, 5, 3, 2], []]), 7)
+  Test.assert_equals(elements_sum([[3, 2, 1, 0], [4, 6, 5, 3, 2], []], 5), 12)
+  Test.assert_equals(elements_sum([[3, 2], [4], []]), 0)
+end
+=end
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry-byebug'
+
+class SubArrayElementsSum < Minitest::Test
+
+=begin
+I: an array of arrays and a number
+0: an integer
+AL:
+  - count the number of subarrays
+  - cycle through the arrays
+    - grab the element from the current subarray, which starts at the array size
+      and is decremented by one each time
+    - if the value is nil, return the default value
+=end
+
+  def elements_sum(arr, d = 0)
+    arr.reverse.each.with_index.inject(0) do |sum, (ary, idx)|
+      sum += (num = ary[idx].nil? ? d : ary[idx])
+    end
+  end
+
+  def test_1
+    # skip
+    assert_equal(16, elements_sum([[3, 2, 1, 0], [4, 6, 5, 3, 2], [9, 8, 7, 4]]))
+  end
+
+  def test_2
+    # skip
+    assert_equal(15, elements_sum([[3], [4, 6, 5, 3, 2], [9, 8, 7, 4]]))
+  end
+
+  def test_3
+    # skip
+    assert_equal(7, elements_sum([[3, 2, 1, 0], [4, 6, 5, 3, 2], []]))
+  end
+
+  def test_4
+    # skip
+    assert_equal(12, elements_sum([[3, 2, 1, 0], [4, 6, 5, 3, 2], []], 5))
+  end
+
+  def test_5
+    # skip
+    assert_equal(0, elements_sum([[3, 2], [4], []]))
+  end
+
+end
